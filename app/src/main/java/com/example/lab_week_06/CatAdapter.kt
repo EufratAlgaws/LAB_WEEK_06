2a7092c2ad1331b9e045b60e894c4332d5ff6f35
Eufrat Algaws <eufrat.ag@gmail.com>
2025-10-15 18:34:50 +0700

    Commit No.2 - Clickable RecyclerView

---
+++ app/src/main/java/com/example/lab_week_06/CatAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.lab_week_06
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.lab_week_06.model.CatModel
+
+class CatAdapter(
+    private val layoutInflater: LayoutInflater,
+    private val imageLoader: ImageLoader,
+    private val onClickListener: OnClickListener? = null
+) : RecyclerView.Adapter<CatViewHolder>() {
+
+    private val cats = mutableListOf<CatModel>()
+
+    fun setData(newCats: List<CatModel>) {
+        cats.clear()
+        cats.addAll(newCats)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CatViewHolder {
+        val view = layoutInflater.inflate(R.layout.item_list, parent, false)
+        return CatViewHolder(view, imageLoader)
+    }
+
+    override fun getItemCount() = cats.size
+
+    override fun onBindViewHolder(holder: CatViewHolder, position: Int) {
+        val item = cats[position]
+        holder.itemView.setOnClickListener { onClickListener?.onItemClick(item) }
+        holder.bindData(item)
+    }
+
+    interface OnClickListener {
+        fun onItemClick(cat: CatModel)
+    }
+}
